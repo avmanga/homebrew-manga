6cce40bfa7ea89db14e16411e074423b4a9e22fa
Paul Smith <code@paulsmithuk.com>
2023-05-02 12:10:33 +0100

    Add gradle@8.0.2

---
+++ Formula/gradle@8.0.2.rb
@@ -0,0 +1,34 @@
+class GradleAT802 < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-8.0.2-all.zip"
+  sha256 "47a5bfed9ef814f90f8debcbbb315e8e7c654109acd224595ea39fca95c5d4da"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://gradle.org/install/"
+    regex(/href=.*?gradle[._-]v?(\d+(?:\.\d+)+)-all\.(?:zip|t)/i)
+  end
+
+  # TODO: Switch to `openjdk` on 8.2 release. 8.0 and 8.1 series cannot be run
+  # on Java 20: https://github.com/gradle/gradle/issues/23488.
+  depends_on "openjdk@17"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin docs lib src]
+    env = Language::Java.overridable_java_home_env("17")
+    (bin/"gradle").write_env_script libexec/"bin/gradle", env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+
+    (testpath/"settings.gradle").write ""
+    (testpath/"build.gradle").write <<~EOS
+      println "gradle works!"
+    EOS
+    gradle_output = shell_output("#{bin}/gradle build --no-daemon")
+    assert_includes gradle_output, "gradle works!"
+  end
+end
